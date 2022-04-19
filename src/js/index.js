65aa54f47fc8c62b57b81d672989ff22163eadd7
Itamar Joire <ijoire7@gmail.com>
2022-04-19 18:33:41 -0300

    Estrutura feita

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,28 @@
-console.log("Dev m3");
+const url = "http://localhost:5000/products";
+const clothingSection = document.querySelector("#clothing");
+
+function getUser() {
+  axios
+    .get(url)
+    .then((response) => {
+      const data = response.data;
+
+      for (var i = 0; i < data.length; i++) {
+        const div = document.createElement("div");
+        div.setAttribute("id", "product");
+
+        div.innerHTML = ` 
+          <img src="${data[i].image}" alt="" />
+          <h3>${data[i].name}</h3>
+          <p class="price">R$ ${data[i].price}</p>
+          <p class="portion">até ${data[i].parcelamento[0]}x de R$${data[i].parcelamento[1]}</p>
+          <a href="#">COMPRAR</a>
+        `;
+
+        clothingSection.appendChild(div);
+      }
+    })
+    .catch((error) => console.log(error));
+}
+
+getUser();
