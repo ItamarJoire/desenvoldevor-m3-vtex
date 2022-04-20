272da44ad42ca97b1a12b44741d0987426d3c19e
Itamar Joire <ijoire7@gmail.com>
2022-04-20 13:58:04 -0300

    alteração dos campos sizes

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,23 @@ const url = "http://localhost:5000/products";
 const containerClothingSection = document.querySelector(".container-clothing");
 const clothingSection = document.querySelector("#clothing");
 const checkColors = document.querySelectorAll(".filters .shirts-op");
+const checkSizes = document.querySelectorAll(".size");
 
 const optionsColors = [];
 
+// FILTRO TAMANHOS
+checkSizes.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    // console.log(evet.)
+  });
+});
+
+// FILTRO CORES
 checkColors.forEach((button) => {
   button.addEventListener("click", (event) => {
     if (button.checked) {
       handleClick(event);
-    }
-    if (!button.checked) {
-      console.log("Entrou no REMOVE");
+    } else {
       handleClickRemove(event);
     }
   });
@@ -30,11 +37,32 @@ function handleClickRemove(event) {
   eventValue = event.target.value;
   for (var i = 0; i <= optionsColors.length; i++) {
     if (optionsColors[i] == eventValue) {
-      console.log("Array com a cor: " + optionsColors[i]);
       optionsColors.splice(i, 1);
       clothingSection.innerHTML = "";
-      console.log("Array SEM a cor: " + optionsColors[i]);
+
       getUser();
+    }
+  }
+}
+
+function call(data) {
+  clothingSection.innerHTML = "";
+  for (var i = 0; i <= optionsColors.length; i++) {
+    for (var j = 0; j < data.length; j++) {
+      if (optionsColors[i] === data[j].color) {
+        const div = document.createElement("div");
+        div.setAttribute("id", "product");
+
+        div.innerHTML = `
+            <img src="${data[j].image}" alt="" />
+            <h3>${data[j].name}</h3>
+            <p class="price">R$ ${data[j].price}</p>
+            <p class="portion">até ${data[j].parcelamento[0]}x de R$${data[j].parcelamento[1]}</p>
+            <a href="#">COMPRAR</a>
+        `;
+
+        clothingSection.appendChild(div);
+      }
     }
   }
 }
@@ -69,28 +97,6 @@ function getUser() {
       }
     })
     .catch((error) => console.log(error));
-}
-
-function call(data) {
-  clothingSection.innerHTML = "";
-  for (var i = 0; i <= optionsColors.length; i++) {
-    for (var j = 0; j < data.length; j++) {
-      if (optionsColors[i] === data[j].color) {
-        const div = document.createElement("div");
-        div.setAttribute("id", "product");
-
-        div.innerHTML = `
-            <img src="${data[j].image}" alt="" />
-            <h3>${data[j].name}</h3>
-            <p class="price">R$ ${data[j].price}</p>
-            <p class="portion">até ${data[j].parcelamento[0]}x de R$${data[j].parcelamento[1]}</p>
-            <a href="#">COMPRAR</a>
-        `;
-
-        clothingSection.appendChild(div);
-      }
-    }
-  }
 }
 
 getUser();
