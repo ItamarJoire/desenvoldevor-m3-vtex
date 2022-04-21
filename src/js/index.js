69216963ba91e5c61fa87619e91dae4725d96f1f
Itamar Joire <ijoire7@gmail.com>
2022-04-21 17:22:43 -0300

    Código filtro cor organizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-const containerClothingSection = document.querySelector(".container-clothing");
 const url = "http://localhost:5000/products";
 const clothingSection = document.querySelector("#clothing");
 const checkColors = document.querySelectorAll(".filters .shirts-op");
@@ -7,13 +6,13 @@ const checkSizes = document.querySelectorAll(".size");
 const collection = [];
 const eventsColors = [];
 
+// Checa se algum filtro de cor foi selecionado
 checkColors.forEach((button) => {
   button.addEventListener("click", (event) => {
     const eventValue = event.target.value;
 
     if (button.checked) {
       eventsColors.push(eventValue);
-
       getUser();
     } else {
       removeProductFilter(eventValue);
@@ -22,12 +21,12 @@ checkColors.forEach((button) => {
   });
 });
 
+// Remove os produtos correspondentes quando se tira a opção de filtro de cor
 function removeProductFilter(eventValue) {
   for (let i = 0; i < collection.length; i++) {
     if (collection[i].color === eventValue) {
       console.log(collection[i]);
       collection.splice(i, 1);
-
       i = -1;
     }
   }
@@ -46,10 +45,12 @@ function removeProductFilter(eventValue) {
 }
 
 function insertFilter(data) {
-  clothingSection.innerHTML = "";
+  // Limpando o array de filtros
   while (collection.length) {
     collection.pop();
   }
+
+  // Inserindo no array de filtros as estruturas filtradas
   for (let i = 0; i < eventsColors.length; i++) {
     data.filter((element) => {
       if (element.color === eventsColors[i]) {
@@ -66,13 +67,9 @@ function insertFilter(data) {
       }
     });
   }
-
-  console.log(eventsColors);
-  // while (eventsColors.length) {
-  //   eventsColors.pop();
-  // }
 }
 
+// Captura os dados da API
 function getUser() {
   axios
     .get(url)
@@ -80,31 +77,47 @@ function getUser() {
       const data = response.data;
 
       if (eventsColors.length == 0) {
-        for (var i = 0; i < data.length; i++) {
-          const div = document.createElement("div");
-          div.setAttribute("id", "product");
-
-          div.innerHTML = `
-            <img src="${data[i].image}" alt="" />
-            <h3>${data[i].name}</h3>
-            <p class="price">R$ ${data[i].price}</p>
-            <p class="portion">até ${data[i].parcelamento[0]}x de R$${data[i].parcelamento[1]}</p>
-            <a href="#">COMPRAR</a>
-          `;
-
-          clothingSection.appendChild(div);
-        }
+        structsProducts.productsAll(data);
         return;
       } else {
         insertFilter(data);
       }
+      structsProducts.filteredProducts();
+    })
+    .catch((error) => console.log(error));
+}
 
-      clothingSection.innerHTML = "";
-      for (var i = 0; i < collection.length; i++) {
-        const div = document.createElement("div");
-        div.setAttribute("id", "product");
+getUser();
 
-        div.innerHTML = `
+// Objeto que contém os métodos que mostra os produtos na view
+const structsProducts = {
+  // Todos os produtos da API
+  productsAll(data) {
+    clothingSection.innerHTML = "";
+    for (var i = 0; i < data.length; i++) {
+      const div = document.createElement("div");
+      div.setAttribute("id", "product");
+
+      div.innerHTML = `
+        <img src="${data[i].image}" alt="" />
+        <h3>${data[i].name}</h3>
+        <p class="price">R$ ${data[i].price}</p>
+        <p class="portion">até ${data[i].parcelamento[0]}x de R$${data[i].parcelamento[1]}</p>
+        <a href="#">COMPRAR</a>
+      `;
+
+      clothingSection.appendChild(div);
+    }
+  },
+
+  // Captura dos produtos com os filtros
+  filteredProducts() {
+    clothingSection.innerHTML = "";
+    for (var i = 0; i < collection.length; i++) {
+      const div = document.createElement("div");
+      div.setAttribute("id", "product");
+
+      div.innerHTML = `
           <img src="${collection[i].image}" alt="" />
           <h3>${collection[i].name}</h3>
           <p class="price">R$ ${collection[i].price}</p>
@@ -112,12 +125,7 @@ function getUser() {
           <a href="#">COMPRAR</a>
         `;
 
-        clothingSection.appendChild(div);
-      }
-
-      console.log(collection);
-    })
-    .catch((error) => console.log(error));
-}
-
-getUser();
+      clothingSection.appendChild(div);
+    }
+  },
+};
